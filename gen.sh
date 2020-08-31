c30804139844a6be9fd32ca85fec51b2848152f0
codekid <ckrackerx15@gmail.com>
2020-08-31 10:17:23 +0530

    gen: modify script to build from scratch

--- gen.sh
+++ gen.sh
@@ -2,18 +2,19 @@
 
 rm -rf ../rubik-mdbook-build
 mkdir ../rubik-mdbook-build && cd ../rubik-mdbook-build
+cd ..
+# rm -rf rubik-mdbook-build/*
+cd rubik-mdbook/
+mdbook build -d ../rubik-mdbook-build
+cd ../rubik-mdbook-build
 git init
 git remote add origin https://github.com/rubikorg/rubikorg.github.io.git
 git fetch --all
 git pull origin master
-
-cd ..
-rm -rf rubik-mdbook-build/*
-cd rubik-mdbook/
-mdbook build -d ../rubik-mdbook-build
-cd rubik-mdbook-build
 git add .
+echo "\n\nCommiting !!"
 git commit -m "mdbook: update docs"
+git push --set-upstream origin master
 
-echo "Cleaning up !!"
+echo "\n\nCleaning up !!"
 rm -rf ../rubik-mdbook-build
