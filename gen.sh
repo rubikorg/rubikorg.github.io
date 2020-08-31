ef85b8f136ffe0b9d6d6cbfc745e71620e8fe90c
codekid <ckrackerx15@gmail.com>
2020-08-31 09:51:41 +0530

    mdbook: update docs

--- gen.sh
+++ gen.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-rm -rf build
-mkdir build && cd build
+rm -rf ../rubik-mdbook-build
+mkdir ../rubik-mdbook-build && cd ../rubik-mdbook-build
 git init
 git remote add origin https://github.com/rubikorg/rubikorg.github.io.git
 git fetch --all
 git pull origin master
 
 cd ..
-rm -rf build/
-mdbook build -d build
-cd build
+rm -rf rubik-mdbook-build/*
+cd rubik-mdbook/
+mdbook build -d ../rubik-mdbook-build
+cd rubik-mdbook-build
 git add .
 git commit -m "mdbook: update docs"
+
+echo "Cleaning up !!"
+rm -rf ../rubik-mdbook-build
